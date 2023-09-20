a9c5dd98a32ab3387934f84b5a43ada541c86dac
mkstarr <starrtennis@gmail.com>
2023-09-20 16:25:47 -0700

    using template but file not found error

--- tutorials/app.py
+++ tutorials/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return 'Web App with Python Flask!'
+    name = 'Rosalia'
+    return render_template('index.html', title='Welcome', username=name)
 
 app.run(host='0.0.0.0', port=81)
